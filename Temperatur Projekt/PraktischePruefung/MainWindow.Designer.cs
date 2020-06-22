f598bfd15579a988cafe7d57ab767d042e7e9e3b
limix <37452162+limix0@users.noreply.github.com>
2020-06-22 17:06:08 +0200

    Add files via upload

---
+++ Temperatur Projekt/PraktischePruefung/MainWindow.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace PraktischePruefung
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.grpBxServerRaum = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblVolt = new System.Windows.Forms.Label();
+            this.lblCOMPort = new System.Windows.Forms.Label();
+            this.cmBxCOMPorts = new System.Windows.Forms.ComboBox();
+            this.SevSegDis_1 = new DmitryBrant.CustomControls.SevenSegmentArray();
+            this.btnStartMessung = new System.Windows.Forms.Button();
+            this.grpBxServerRaum.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpBxServerRaum
+            // 
+            this.grpBxServerRaum.Controls.Add(this.SevSegDis_1);
+            this.grpBxServerRaum.Controls.Add(this.cmBxCOMPorts);
+            this.grpBxServerRaum.Controls.Add(this.lblCOMPort);
+            this.grpBxServerRaum.Controls.Add(this.lblVolt);
+            this.grpBxServerRaum.Location = new System.Drawing.Point(12, 111);
+            this.grpBxServerRaum.Name = "grpBxServerRaum";
+            this.grpBxServerRaum.Size = new System.Drawing.Size(409, 153);
+            this.grpBxServerRaum.TabIndex = 1;
+            this.grpBxServerRaum.TabStop = false;
+            this.grpBxServerRaum.Text = "Temperatur";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(91, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 93);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblVolt
+            // 
+            this.lblVolt.AutoSize = true;
+            this.lblVolt.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVolt.Location = new System.Drawing.Point(299, 20);
+            this.lblVolt.Name = "lblVolt";
+            this.lblVolt.Size = new System.Drawing.Size(101, 73);
+            this.lblVolt.TabIndex = 0;
+            this.lblVolt.Text = "°V";
+            // 
+            // lblCOMPort
+            // 
+            this.lblCOMPort.AutoSize = true;
+            this.lblCOMPort.Location = new System.Drawing.Point(315, 99);
+            this.lblCOMPort.Name = "lblCOMPort";
+            this.lblCOMPort.Size = new System.Drawing.Size(53, 13);
+            this.lblCOMPort.TabIndex = 1;
+            this.lblCOMPort.Text = "COM Port";
+            // 
+            // cmBxCOMPorts
+            // 
+            this.cmBxCOMPorts.FormattingEnabled = true;
+            this.cmBxCOMPorts.Location = new System.Drawing.Point(312, 115);
+            this.cmBxCOMPorts.Name = "cmBxCOMPorts";
+            this.cmBxCOMPorts.Size = new System.Drawing.Size(66, 21);
+            this.cmBxCOMPorts.TabIndex = 2;
+            this.cmBxCOMPorts.SelectedIndexChanged += new System.EventHandler(this.cmBxCOMPorts_SelectedIndexChanged);
+            // 
+            // SevSegDis_1
+            // 
+            this.SevSegDis_1.ArrayCount = 3;
+            this.SevSegDis_1.ColorBackground = System.Drawing.Color.DimGray;
+            this.SevSegDis_1.ColorDark = System.Drawing.Color.Transparent;
+            this.SevSegDis_1.ColorLight = System.Drawing.Color.Chartreuse;
+            this.SevSegDis_1.DecimalShow = true;
+            this.SevSegDis_1.ElementPadding = new System.Windows.Forms.Padding(4, 4, 15, 4);
+            this.SevSegDis_1.ElementWidth = 10;
+            this.SevSegDis_1.ItalicFactor = 0F;
+            this.SevSegDis_1.Location = new System.Drawing.Point(28, 19);
+            this.SevSegDis_1.Name = "SevSegDis_1";
+            this.SevSegDis_1.Size = new System.Drawing.Size(265, 122);
+            this.SevSegDis_1.TabIndex = 5;
+            this.SevSegDis_1.TabStop = false;
+            this.SevSegDis_1.Value = "5.12";
+            // 
+            // btnStartMessung
+            // 
+            this.btnStartMessung.Location = new System.Drawing.Point(13, 271);
+            this.btnStartMessung.Name = "btnStartMessung";
+            this.btnStartMessung.Size = new System.Drawing.Size(408, 39);
+            this.btnStartMessung.TabIndex = 4;
+            this.btnStartMessung.Text = "Messung starten";
+            this.btnStartMessung.UseVisualStyleBackColor = true;
+            this.btnStartMessung.Click += new System.EventHandler(this.btnStartMessung_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(435, 322);
+            this.Controls.Add(this.btnStartMessung);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.grpBxServerRaum);
+            this.Name = "MainWindow";
+            this.Text = "Temperatur";
+            this.grpBxServerRaum.ResumeLayout(false);
+            this.grpBxServerRaum.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox grpBxServerRaum;
+        private DmitryBrant.CustomControls.SevenSegmentArray SevSegDis_1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cmBxCOMPorts;
+        private System.Windows.Forms.Label lblCOMPort;
+        private System.Windows.Forms.Label lblVolt;
+        private System.Windows.Forms.Button btnStartMessung;
+    }
+}
+
---
+++ Temperatur Projekt/PraktischePruefung/MainWindow.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Collections;
+using System.IO.Ports;
+
+namespace PraktischePruefung
+{
+    
+    public partial class MainWindow : Form
+    { 
+        // SQL Variablen
+        String connectionString = "SERVER=localhost;DATABASE=temperatur;UID=root;PASSWORD=;"; // Verbindung zur lokalen Datenbank
+        MySqlConnection connection { get; set;  }
+        MySqlConnection connection2 { get; set; }
+        MySqlCommand command = null;
+        MySqlDataReader dataReader;
+        Boolean connectedToDatabase = false;
+
+        //  Serielle Schnitstelle
+        private SerialPort sPort1 = new SerialPort();
+        public delegate void AddDataDelegate(String myString);
+        public AddDataDelegate myDelegate;
+        String outPutVoltage = ""; // Voltage wird auf 0 gesetzt, da noch keine vorliegt
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetAvaialblePorts();
+            ConnectToDatabase();
+            LoadDataFromDatabase();
+            OpenSerialPort();
+            this.myDelegate = new AddDataDelegate(AddDataMethod);                      
+        }
+        void OpenSerialPort() // Serielle Schnittstelle
+        {
+            if ( this.cmBxCOMPorts.SelectedIndex != -1)
+            {
+                sPort1.Close();
+                sPort1 = new SerialPort(this.cmBxCOMPorts.SelectedItem.ToString(), 9600, Parity.None, 8, StopBits.One);
+                sPort1.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+                sPort1.Open(); // Serielle Schnittstelle öffnen
+            }
+           
+        }
+        void GetAvaialblePorts() // Verfügbare Ports erkennen und Box anzeigen
+        {
+            String[] ports = SerialPort.GetPortNames();
+            cmBxCOMPorts.Items.AddRange(ports);
+        }
+        private void SerialPort_DataReceived(object sender,SerialDataReceivedEventArgs e) // Methode ob Daten empfangen wurden
+        {
+            SerialPort sp = (SerialPort)sender;
+            sp.NewLine = ";"; 
+            string data = sp.ReadLine(); 
+            SevSegDis_1.Invoke(myDelegate, data); // Methode um die Variableneinzelteile voneinander zu trennen
+        }
+        public void AddDataMethod(String myString) 
+        {
+            // Dezimal Nummer Umwandlung
+            this.outPutVoltage = "";
+            int tempInt = 0;
+            tempInt  = Convert.ToInt32(myString) / 1000;
+            this.outPutVoltage += tempInt;
+            this.outPutVoltage += ".";
+            tempInt = (Convert.ToInt32(myString) % 1000) / 100;
+            this.outPutVoltage += tempInt;
+            tempInt = (Convert.ToInt32(myString) % 100) / 10;
+            this.outPutVoltage += tempInt;
+            this.SevSegDis_1.Value = outPutVoltage;
+            this.SevSegDis_1.Update(); // 7 Segment Anzeige aktualisieren nachdem der Wert auf Dezimal umgewandelt wurde
+
+        }
+        
+        public void ConnectToDatabase() // Verbindung mit der Datenbank aufbauen
+        {
+            SevSegDis_1.Value = null; // 7 Segment Anzeige zurücksetzen auf Standart
+            SevSegDis_1.Update();
+
+            connection = new MySqlConnection(connectionString);
+            connection2 = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open(); // Verbindung zut Datenbank aufbauen
+                connection2.Open();
+                connectedToDatabase = true;
+            }
+            catch
+            {
+                MessageBox.Show("Fehlerhafte Verbindung zur Datenbank."); // Kann keine Verbindung aufbauen
+            }
+        }
+        public void DisconnectFromDatabase() // Datenbank Verbindung trennen
+        {
+            if (connectedToDatabase == true) // Falls Verbindung vorhanden
+            {
+                connection.Close();
+                connection2.Close();
+            }
+            else // Falls keine Verbindung vorhanden ist
+            {
+                MessageBox.Show("Nicht mit Datenbank verbunden.");
+            }
+        }
+        public void LoadDataFromDatabase() // Aus der Datenbank lesen
+        {
+            if (connectedToDatabase)
+            {
+                command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM temperaturen;";
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    String temperatur = Convert.ToString(dataReader[0]); // Wert speichern in Variable
+                }
+                dataReader.Close();
+            }            
+        }
+
+        public ArrayList GetSNs() // Serielle Nummern 
+        {
+            if (connectedToDatabase)
+            {
+                ArrayList sns = new ArrayList();
+                this.command.CommandText = "SELECT * FROM temperatur";
+                this.dataReader = this.command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    sns.Add(dataReader[0].ToString());
+                }
+                dataReader.Close();
+                return sns;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void cmBxCOMPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OpenSerialPort(); // Serielle Schnitstelle öffnen
+        }
+
+        private void btnStartMessung_Click(object sender, EventArgs e) // Wenn Messung starten gedrückt
+        {
+            if (connectedToDatabase)
+            {
+                command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM temperaturen;"; // Datenbank auslesen
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    String temperatur = Convert.ToString(dataReader[0]); // Wert aus DB speichern
+                    SevSegDis_1.Value = temperatur;
+                    SevSegDis_1.Update(); // Temperatur aktualisieren
+                }
+                dataReader.Close();
+            }
+            else
+            {
+                MessageBox.Show("Nicht mit Datenbank verbunden.");
+            }
+        }
+    }
+    interface IMainWindow // Main Window
+    {
+        ArrayList GetRooms();
+        ArrayList GetSNs();
+    }
+}
